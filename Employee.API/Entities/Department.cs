f79667f9d63df380b2911ddde3398045aadd09f1
Simuzeche Kaluwa <Skaluwa@nitel.me>
2021-01-22 15:05:27 +0200

    Added Employee and Department Entities
    Removed Default WeatherForecastController and WeatherForecast class

---
+++ Employee.API/Entities/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee.API.Entities
+{
+    public class Department
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string DepartmentName { get; set; }
+
+        
+       
+    }
+}
---
+++ Employee.API/Entities/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee.API.Entities
+{
+    public class Employee
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTimeOffset DateOfBirth { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string MainCategory { get; set; }
+
+        public Department Department { get; set; }
+    }
+}
