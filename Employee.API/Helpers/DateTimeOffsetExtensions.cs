5245902434531d546e74e976dfcdbdea9053cf8a
Simuzeche Kaluwa <Skaluwa@nitel.me>
2021-01-26 11:03:06 +0200

    Implemented GetAge helper function

---
+++ Employee.API/Helpers/DateTimeOffsetExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee.API.Helpers
+{
+    public static class DateTimeOffsetExtensions
+    {
+        public static int GetCurrentAge(this DateTimeOffset dateTimeOffset)
+        {
+            var currentAge = DateTime.UtcNow;
+            int age = currentAge.Year - dateTimeOffset.Year;
+
+            if(currentAge < dateTimeOffset.AddYears(age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+}
