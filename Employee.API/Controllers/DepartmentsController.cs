9e9b57a3301a6b99fee725d8aad8f0b424d6ee00
Simuzeche Kaluwa <Skaluwa@nitel.me>
2021-01-27 16:13:23 +0200

    Added Departments Controller

---
+++ Employee.API/Controllers/DepartmentsController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Employee.API.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee.API.Controllers
+{
+    [Route("api/employess/{departmentId}/departments")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IMapper _mapper;
+
+        public DepartmentsController(IEmployeeRepository employeeRepository, IMapper mapper)
+        {
+            _employeeRepository = employeeRepository ??
+                throw new ArgumentNullException(nameof(employeeRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+    }
+}
