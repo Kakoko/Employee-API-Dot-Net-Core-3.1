027b5b7e611b469f4a1f5b5d06a28186cf1d00a3
Simuzeche Kaluwa <Skaluwa@nitel.me>
2021-01-27 16:16:09 +0200

    Added Department DTO

---
+++ Employee.API/Models/DepartmentDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee.API.Models
+{
+    public class DepartmentDTO
+    {
+        public string DepartmentName { get; set; }
+        public ICollection<Entities.Employee> Employees { get; set; }
+         = new List<Entities.Employee>();
+    }
+}
